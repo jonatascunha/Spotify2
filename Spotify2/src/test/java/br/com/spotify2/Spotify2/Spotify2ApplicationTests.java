674e868ac0bd52135a4767adee905f908c7bd965
jonatascunha <jonatasfielcardoso@gmail.com>
2022-04-26 20:02:19 -0300

    criando pacotes

---
+++ Spotify2/src/test/java/br/com/spotify2/Spotify2/Spotify2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spotify2.Spotify2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spotify2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
